e037a9be3e5d5b3e157a22d580f4a6d5301da463
Russell Gordon <rgordon@rsgc.on.ca>
2017-04-07 08:44:43 -0400

    Got basic idea of shapes being drawn (circles or squares).

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -32,9 +32,26 @@ for x in stride(from: 25, through: 475, by: 50){
     // This loop makes a single column, bottom to top
     for y in stride(from: 25, through: 475, by: 50) {
         
-        // Draw the shapes
-        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
-        canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
+        // Generate a random number to help with shape selection
+        var shapeSelector = random(from: 0, toButNotIncluding: 2)
+        
+        // Loop five times to create shapes with decreasing size
+        for size in stride(from: 50, through: 10, by: -10) {
+            
+            // Depending on random number, draw squares or circles
+            if shapeSelector == 0 {
+                
+                canvas.drawRectangle(centreX: x, centreY: y, width: size, height: size)
+                
+            } else {
+                
+                canvas.drawEllipse(centreX: x, centreY: y, width: size, height: size)
+                
+            }
+            
+        }
+        
+        
     }
 }
 
