7b95a9bb57c5cdd2406fb6ba5708c5abaf471237
Student <Max@MacBook-Pro-423-Livingston.local>
2017-04-16 15:47:53 -0400

    I have been able to finalize my design, adding conditional statements

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design Two.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design Two.xcplaygroundpage/Contents.swift
@@ -21,7 +21,7 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-// Generate a grid
+// Generating a grid
 canvas.drawShapesWithFill = true
 canvas.defaultBorderWidth = 1
 canvas.drawShapesWithBorders = false
@@ -33,19 +33,19 @@ for x in stride(from: 25, through: 475, by: 50){
     for y in stride(from: 25, through: 475, by: 50) {
         let hue = random(from: 0, toButNotIncluding: 360)
        
-        // Draw the shapes
+        // Draw the circles 10 by 10
         canvas.drawEllipse(centreX: x, centreY: y, width: 25, height: 25)
         canvas.fillColor = Color(hue: hue, saturation: 80, brightness: 100, alpha: 100)
         
         
-       // Drawing bigger circles ontop of others
+    // If statement regarding whether to draw rectangles or not.
        
         if hue > 180 {
         canvas.drawRectangle(bottomLeftX: y, bottomLeftY: x, width: 10, height: 50)
         canvas.drawRectangle(bottomLeftX: x, bottomLeftY: y, width: 50, height: 10)
             }
-            
-         else { canvas.drawEllipse(centreX: y, centreY: x, width: 25, height: 15) }
+    // Putting an else statement showing mini-circles inside of the circles with a hue above 180
+         else { canvas.drawEllipse(centreX: y, centreY: x, width: 15, height: 15) }
         
         
         }
